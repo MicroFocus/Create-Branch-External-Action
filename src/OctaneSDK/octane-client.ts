60e5fc303e6ed1907be9c4fb3905a08fb9cda97a
stancial <alexander.stanciu@microfocus.com>
2021-06-07 09:28:22 +0300

    Added License

--- src/OctaneSDK/octane-client.ts
+++ src/OctaneSDK/octane-client.ts
@@ -1,5 +1,5 @@
 /*!
- * (c) 2016-2021 EntIT Software LLC, a Micro Focus company
+ * © Copyright 2021 Micro Focus or one of its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/OctaneSDK/octane-scope.ts
+++ src/OctaneSDK/octane-scope.ts
@@ -1,5 +1,5 @@
 /*!
- * (c) 2016-2019 EntIT Software LLC, a Micro Focus company
+ * © Copyright 2021 Micro Focus or one of its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 
 import {Got, HTTPError} from 'got';
 import {compact} from 'lodash';
